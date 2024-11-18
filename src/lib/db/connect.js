5826d616f59f40b2626b9ce7c76db0450705f4e3
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2024-11-18 21:05:25 +0100

    updates for files to deploy on vercel

--- src/lib/db/connect.js
+++ src/lib/db/connect.js
@@ -19,8 +19,7 @@ export async function dbConnect() {
 
   if (!cached.promise) {
     const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      bufferCommands: false,
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
